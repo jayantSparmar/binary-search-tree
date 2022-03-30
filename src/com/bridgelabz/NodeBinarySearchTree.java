f0f2d4aae6a7a48724c0d17a4fb87da451159a46
jayant <jayantparmar420@gmail.com>
2022-03-30 20:10:02 +0530

    added the insert node in this program.

--- src/com/bridgelabz/NodeBinarySearchTree.java
+++ src/com/bridgelabz/NodeBinarySearchTree.java
@@ -1,3 +1,28 @@
 package com.bridgelabz;
 
+class Node {
+    int data;
+    Node left, right;
+}
+public class NodeBinarySearchTree {
+    public Node insert(Node node, int data) {
+        if (node == null) {
+            return createNode(data);
+        }
+        if (data < node.data) {
+            node.left = insert(node.left, data);
 
+        } else if (data > node.data) {
+            node.right = insert(node.right, data);
+        }
+        return node;
+    }
+
+    private Node createNode(int data) {
+        Node root = new Node();
+        root.data = data;
+        root.left = null;
+        root.right = null;
+        return root;
+    }
+}
