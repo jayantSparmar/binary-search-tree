4e04af129e49012a291f9200324650ad227b1866
jayant <jayantparmar420@gmail.com>
2022-03-30 20:05:28 +0530

    added the welcome message in the binary search tree program.

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the binary search tree.");
+    }
+    }
